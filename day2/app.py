37cf6f7893bea04a7a31af1044b94d77cb7c1a13
Peter Byfield <peter.byfield@enable.com>
2019-12-05 13:15:21 +0000

    refactor day 2

--- day2/app.py
+++ day2/app.py
@@ -1,4 +1,5 @@
 import os
+import copy
 
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
@@ -13,48 +14,49 @@ def multiply_values(lst, idx_x, idx_y, idx_insert):
     lst[idx_insert] = lst[idx_x] * lst[idx_y]
 
 
-def run(noun, verb):
+def run(code, noun, verb):
+    code = copy.deepcopy(code)
+    code[1] = noun
+    code[2] = verb
+
+    idx = 0
+
+    while (True):
+        operator = code[idx]
+
+        if (operator == 1):
+            add_values(code, code[idx + 1], code[idx + 2], code[idx + 3])
+        elif (operator == 2):
+            multiply_values(code, code[idx + 1],
+                            code[idx + 2], code[idx + 3])
+        elif (operator == 99):
+            break
+        else:
+            raise Exception('Operator {} not supported'.format(operator))
+
+        idx += 4
+
+    return code[0]
+
+
+def compute_noun_and_verb(code, required_output):
+    for noun in range(100):
+        for verb in range(100):
+            output = run(code, noun, verb)
+
+            if (output == required_output):
+                return (noun, verb)
+
+
+if __name__ == "__main__":
     with open(input_path) as f:
         line = f.readline()
         code = list(map(lambda x: int(x), line.strip().split(',')))
-        code[1] = noun
-        code[2] = verb
 
-        idx = 0
+        print('Part 1')
+        print('Output = {}'.format(run(code, 12, 2)))
 
-        while (True):
-            operator = code[idx]
+        noun, verb = compute_noun_and_verb(code, 19690720)
 
-            if (operator == 1):
-                add_values(code, code[idx + 1], code[idx + 2], code[idx + 3])
-            elif (operator == 2):
-                multiply_values(code, code[idx + 1],
-                                code[idx + 2], code[idx + 3])
-            elif (operator == 99):
-                break
-            else:
-                raise Exception('Operator {} not supported'.format(operator))
-
-            idx += 4
-
-        return code[0]
-
-
-print('Part 1')
-print('Output = {}'.format(run(12, 2)))
-
-done = False
-
-for noun in range(100):
-    for verb in range(100):
-        out = run(noun, verb)
-
-        if (out == 19690720):
-            done = True
-            break
-
-    if done:
-        break
-
-print('Part 2')
-print('Noun = {}, Verb = {}'.format(noun, verb))
+        print('Part 2')
+        print('Noun = {}, Verb = {}'.format(noun, verb))
