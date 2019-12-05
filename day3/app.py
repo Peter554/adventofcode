48dfe70f9a9e137220e16d9d60d15db5a7e9f21e
Peter <byfield554@gmail.com>
2019-12-05 21:52:00 +0000

    day 3 part 1 done

--- day3/app.py
+++ day3/app.py
@@ -1,32 +1,33 @@
 import os
+from typing import List, Tuple, NamedTuple
 
 
-def apply_command(lst, command):
+def apply_command(lst: List[Tuple[int, int]], command: str) -> None:
     previous = lst[-1]
 
     direction = command[0]
     amount = int(command[1:])
 
     if direction == 'u':
-        def action():
-            lst.append((previous[0] + 1, previous[1]))
+        def action(i):
+            lst.append((previous[0] + i, previous[1]))
     elif direction == 'r':
-        def action():
-            lst.append((previous[0], previous[1] + 1))
+        def action(i):
+            lst.append((previous[0], previous[1] + i))
     elif direction == 'd':
-        def action():
-            lst.append((previous[0] - 1, previous[1]))
+        def action(i):
+            lst.append((previous[0] - i, previous[1]))
     elif direction == 'l':
-        def action():
-            lst.append((previous[0], previous[1] - 1))
+        def action(i):
+            lst.append((previous[0], previous[1] - i))
     else:
         raise Exception('Direction {} not supported'.format(direction))
 
-    for _ in range(amount):
-        action()
+    for i in range(amount):
+        action(i + 1)
 
 
-def build_wire_coordinates(text: str):
+def build_wire(text: str) -> List[Tuple[int, int]]:
     commands = text.strip().lower().split(',')
 
     out = [(0, 0)]
@@ -39,6 +40,29 @@ def build_wire_coordinates(text: str):
     return out
 
 
+class BoundingBox(NamedTuple):
+    x_max: int
+    x_min: int
+    y_max: int
+    y_min: int
+
+
+def get_bounding_box(wire: List[Tuple[int, int]]) -> BoundingBox:
+    x_max = max(map(lambda x: x[0], wire))
+    x_min = min(map(lambda x: x[0], wire))
+    y_max = max(map(lambda x: x[1], wire))
+    y_min = min(map(lambda x: x[1], wire))
+    return BoundingBox(x_max=x_max, x_min=x_min, y_max=y_max, y_min=y_min)
+
+
+def intersect_bounding_boxes(b1: BoundingBox, b2: BoundingBox) -> BoundingBox:
+    x_max = min([b1.x_max, b2.x_max])
+    x_min = max([b1.x_min, b2.x_min])
+    y_max = min([b1.y_max, b2.y_max])
+    y_min = max([b1.y_min, b2.y_min])
+    return BoundingBox(x_max=x_max, x_min=x_min, y_max=y_max, y_min=y_min)
+
+
 if __name__ == "__main__":
     this_dir = os.path.dirname(os.path.abspath(__file__))
     input_path = os.path.join(this_dir, 'input.txt')
@@ -47,12 +71,21 @@ if __name__ == "__main__":
         wire_1_raw = f.readline()
         wire_2_raw = f.readline()
 
-        wire_1 = build_wire_coordinates(wire_1_raw)
-        wire_2 = build_wire_coordinates(wire_2_raw)
+        wire_1 = build_wire(wire_1_raw)
+        wire_2 = build_wire(wire_2_raw)
 
-        def in_wire_2(x):
-            return any(map(lambda y: y[0] == x[0] and y[1] == x[1], wire_2))
+        bounding_1 = get_bounding_box(wire_1)
+        bounding_2 = get_bounding_box(wire_2)
+        bounding = intersect_bounding_boxes(bounding_1, bounding_2)
 
-        common = filter(in_wire_2, wire_1)
+        def filter_func(x: Tuple[int, int]) -> bool:
+            return x[0] < bounding.x_max and x[0] > bounding.x_min and x[1] < bounding.y_max and x[1] > bounding.y_min
 
-        print(list(common))
+        wire_1 = list(filter(filter_func, wire_1))
+        wire_2 = list(filter(filter_func, wire_2))
+
+        common = [x for x in wire_1 if x in wire_2]
+        manhattan = list(map(lambda x: abs(x[0]) + abs(x[1]), common))
+
+        print('Part 1')
+        print('Distance = {}'.format(min(manhattan)))
