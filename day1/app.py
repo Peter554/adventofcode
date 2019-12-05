1545d0b6e68e12e2b87088c90bf1f2c0db95fc06
Peter Byfield <peter.byfield@enable.com>
2019-12-05 09:03:29 +0000

    day 1 done

--- day1/app.py
+++ day1/app.py
@@ -1,1 +1,43 @@
-print('hello world')
+import os
+import math
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+input_path = os.path.join(this_dir, 'input.txt')
+
+
+def calculate_fuel_for_mass(mass):
+    return math.floor(mass / 3) - 2
+
+
+total_fuel = 0
+
+with open(input_path) as f:
+    for line in f:
+        module_mass = float(line)
+        module_fuel = calculate_fuel_for_mass(module_mass)
+        total_fuel += module_fuel
+
+print('Part 1')
+print('Total fuel: {}'.format(total_fuel))
+
+
+def calculate_fuel_for_mass_2(mass):
+    fuel = math.floor(mass / 3) - 2
+
+    if (fuel > 0):
+        return fuel + calculate_fuel_for_mass_2(fuel)
+
+    return 0
+
+
+total_fuel = 0
+
+
+with open(input_path) as f:
+    for line in f:
+        module_mass = float(line)
+        module_fuel = calculate_fuel_for_mass_2(module_mass)
+        total_fuel += module_fuel
+
+print('Part 2')
+print('Total fuel: {}'.format(total_fuel))
