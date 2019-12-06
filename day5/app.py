16ffd0b72d32d6ebd13d7dfed7bfade2249e79d5
Peter Byfield <peter.byfield@enable.com>
2019-12-06 12:57:26 +0000

    day 5 part 1

--- day5/app.py
+++ day5/app.py
@@ -1,2 +1,15 @@
+import os
+
+from intcode import IntCode
+
 if __name__ == "__main__":
-    print("hello day 5")
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    input_path = os.path.join(this_dir, 'input.txt')
+
+    with open(input_path) as f:
+        raw_code = f.readline()
+        intcode = IntCode(raw_code)
+        output = intcode.run(1)
+
+        print('Part 1')
+        print('Output = {}'.format(output))
