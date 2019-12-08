8c6ad7d7c2f55c54aa25fd5695940a199a350fbe
Peter <byfield554@gmail.com>
2019-12-08 10:13:16 +0000

    done day 8

--- day8/app.py
+++ day8/app.py
@@ -1,5 +1,26 @@
 import os
 import re
+import functools
+
+
+def build_layers(raw_data, width, height):
+    size = width * height
+    layers = [raw_data[i:i+size] for i in range(0, len(raw_data), size)]
+    return [[int(x) for x in layer] for layer in layers]
+
+
+def apply_layer(img, layer):
+    for idx in range(len(img)):
+        if img[idx] == 2:
+            img[idx] = layer[idx]
+    return img
+
+
+def decode(raw_data, width, height):
+    layers = build_layers(raw_data, width, height)
+    img = functools.reduce(apply_layer, layers, [2] * len(layers[0]))
+    return [img[i:i+width] for i in range(0, len(img), width)]
+
 
 if __name__ == "__main__":
     this_dir = os.path.dirname(os.path.abspath(__file__))
@@ -7,13 +28,20 @@ if __name__ == "__main__":
 
     with open(input_path) as f:
         raw_data = f.readline()
-        layers = [raw_data[i:i+150] for i in range(0, len(raw_data), 150)]
-        zeros_re = re.compile(r"0")
-        ones_re = re.compile(r"1")
-        twos_re = re.compile(r"2")
-        zeros_per_layer = list(map(lambda x: len(zeros_re.findall(x)), layers))
+        layers = build_layers(raw_data, 26, 6)
+
+        def count_zeros(layer):
+            return len([x for x in layer if x == 0])
+        zeros_per_layer = [count_zeros(layer) for layer in layers]
         idx = zeros_per_layer.index(min(zeros_per_layer))
-        ones = len(ones_re.findall(layers[idx]))
-        twos = len(twos_re.findall(layers[idx]))
+        ones = len([x for x in layers[idx] if x == 1])
+        twos = len([x for x in layers[idx] if x == 2])
+
         print("Part 1")
         print(f"Answer = {ones * twos}")
+
+        img = decode(raw_data, 25, 6)
+
+        print("Part 2")
+        for row in img:
+            print(row)
---
+++ day8/test_examples.py
@@ -0,0 +1,7 @@
+from app import decode
+
+
+def test_example_1():
+    raw_data = '0222112222120000'
+    decoded = decode(raw_data, 2, 2)
+    assert decoded == [[0, 1], [1, 0]]
