ae52d50ae0e5af57bb4f0a3d89985f60f01267e6
Peter Byfield <byfield554@gmail.com>
2019-12-08 16:42:06 +0000

    Refactor/2018 day5 (#10)
    * day 5 done

---
+++ 2018/day5/app.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/peter554/adventofcode/2018/common"
+)
+
+func main() {
+	rawdata := common.Readlines("./input.txt")[0]
+	units := strings.Split(rawdata, "")
+	fmt.Println("Part 1")
+	fmt.Printf("Lenght = %d\n", getlenghtafterreaction(units))
+	fmt.Println("Part 2")
+	fmt.Printf("Minimum lenght = %d\n", getlenghtafterreaction2(units))
+}
+
+func getlenghtafterreaction(units []string) int {
+	return len(react(units))
+}
+
+func getlenghtafterreaction2(units []string) int {
+	min := -1
+	alphabet := getalphabet()
+	for _, letter := range alphabet {
+		reacted := react(removeunit(units, letter))
+		if min == -1 || len(reacted) < min {
+			min = len(reacted)
+		}
+	}
+	return min
+}
+
+func react(units []string) []string {
+	for {
+		didreact := false
+		for idx, _ := range units {
+			if idx == 0 {
+				continue
+			}
+			sametype := aresametype(units[idx-1], units[idx])
+			oppositepolarity := areoppositepolarity(units[idx-1], units[idx])
+			if sametype && oppositepolarity {
+				units = removepair(units, idx-1)
+				didreact = true
+				break
+			}
+		}
+		if !didreact {
+			return units
+		}
+	}
+}
+
+func aresametype(a string, b string) bool {
+	return strings.ToLower(a) == strings.ToLower(b)
+}
+
+func islower(s string) bool {
+	return s == strings.ToLower(s)
+}
+
+func isupper(s string) bool {
+	return s == strings.ToUpper(s)
+}
+
+func areoppositepolarity(a string, b string) bool {
+	return (islower(a) && isupper(b)) || (isupper(a) && islower(b))
+}
+
+func removepair(units []string, at int) []string {
+	units = copyunits(units)
+	return append(units[:at], units[at+2:]...)
+}
+
+func removeunit(units []string, unit string) []string {
+	out := make([]string, 0)
+	for _, v := range units {
+		if strings.ToLower(v) == strings.ToLower(unit) {
+			continue
+		}
+		out = append(out, v)
+	}
+	return out
+}
+
+func getalphabet() []string {
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	return strings.Split(alphabet, "")
+}
+
+// TODO Why is this required?
+func copyunits(units []string) []string {
+	out := make([]string, len(units))
+	copy(out, units)
+	return out
+}
