382921fd05faa751ec99d180d76d402c15b0b3d6
Peter <byfield554@gmail.com>
2019-12-08 09:10:40 +0000

    init day 8

---
+++ day8/app.py
@@ -0,0 +1,8 @@
+import os
+
+if __name__ == "__main__":
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    input_path = os.path.join(this_dir, "input.txt")
+
+    with open(input_path) as f:
+        print(f.readline())
