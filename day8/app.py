f0cf95b671d5563453e303a6da8068469fdd13f2
Peter <byfield554@gmail.com>
2019-12-08 09:24:42 +0000

    part 1 done

--- day8/app.py
+++ day8/app.py
@@ -1,8 +1,19 @@
 import os
+import re
 
 if __name__ == "__main__":
     this_dir = os.path.dirname(os.path.abspath(__file__))
     input_path = os.path.join(this_dir, "input.txt")
 
     with open(input_path) as f:
-        print(f.readline())
+        raw_data = f.readline()
+        layers = [raw_data[i:i+150] for i in range(0, len(raw_data), 150)]
+        zeros_re = re.compile(r"0")
+        ones_re = re.compile(r"1")
+        twos_re = re.compile(r"2")
+        zeros_per_layer = list(map(lambda x: len(zeros_re.findall(x)), layers))
+        idx = zeros_per_layer.index(min(zeros_per_layer))
+        ones = len(ones_re.findall(layers[idx]))
+        twos = len(twos_re.findall(layers[idx]))
+        print("Part 1")
+        print(f"Answer = {ones * twos}")
