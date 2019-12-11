c6dcd775dc9ef5f009045d68317177bbda4e7b7c
Peter Byfield <peter.byfield@enable.com>
2019-12-11 17:34:19 +0000

    asyncio

--- 2019/day09/app.py
+++ 2019/day09/app.py
@@ -1,24 +1,27 @@
 import os
-import queue
+import asyncio
 
 from intcode import IntCode
 
 
-def run_intcode(raw_code, inputs=[]):
-    input_queue = queue.SimpleQueue()
+async def run_intcode(raw_code, inputs=[]):
+    input_queue = asyncio.Queue()
     computer = IntCode(raw_code, input_queue)
     for value in inputs:
-        input_queue.put(value)
-    return computer.run()
+        await input_queue.put(value)
+    return await computer.run()
 
 
-if __name__ == '__main__':
+async def main():
     this_dir = os.path.dirname(os.path.abspath(__file__))
     input_path = os.path.join(this_dir, 'input.txt')
 
     with open(input_path) as f:
         raw_code = f.readline()
         print('Part 1')
-        print(f'Keycode = {run_intcode(raw_code, [1])}')
+        print(f'Keycode = {await run_intcode(raw_code, [1])}')
         print('Part 2')
-        print(f'Keycode = {run_intcode(raw_code, [2])}')
+        print(f'Keycode = {await run_intcode(raw_code, [2])}')
+
+if __name__ == '__main__':
+    asyncio.run(main())
