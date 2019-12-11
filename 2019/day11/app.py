eab741225001e0a5a56fae07e7d251b873a0ee41
Peter Byfield <peter.byfield@enable.com>
2019-12-11 08:05:29 +0000

    init

---
+++ 2019/day11/app.py
@@ -0,0 +1,16 @@
+import os
+import queue
+
+from intcode import IntCode
+
+
+if __name__ == '__main__':
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    input_path = os.path.join(this_dir, 'input.txt')
+
+    with open(input_path) as f:
+        raw_code = f.readline()
+        print('Part 1')
+        print(f'')
+        print('Part 2')
+        print(f'')
