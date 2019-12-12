b43bdee3c880b7bfe93e0820462dfaf49cc47e64
Peter Byfield <peter.byfield@enable.com>
2019-12-12 12:23:28 +0000

    theory

--- 2019/day12/test_moon.py
+++ 2019/day12/test_moon.py
@@ -1,31 +1,17 @@
-from moon import Moon
+import pytest
 
 from coordinate import Coordinate
+from moon import Moon
 
 
-def test_moon_1():
-    raw_data = '<x=-2, y=9, z=-5>'
+@pytest.mark.parametrize('x,y,z', [
+    (-2, 9, -5),
+    (16, 19, 6),
+    (0, 3, 6),
+    (11, 0, 11)
+])
+def test_moon_init(x, y, z):
+    raw_data = f'<x={x}, y={y}, z={z}>'
     sut = Moon(raw_data)
-    assert sut.p == Coordinate(-2, 9, -5)
-    assert sut.v == Coordinate(0, 0, 0)
-
-
-def test_moon_2():
-    raw_data = '<x=16, y=19, z=9>'
-    sut = Moon(raw_data)
-    assert sut.p == Coordinate(16, 19, 9)
-    assert sut.v == Coordinate(0, 0, 0)
-
-
-def test_moon_3():
-    raw_data = '<x=0, y=3, z=6>'
-    sut = Moon(raw_data)
-    assert sut.p == Coordinate(0, 3, 6)
-    assert sut.v == Coordinate(0, 0, 0)
-
-
-def test_moon_4():
-    raw_data = '<x=11, y=0, z=11>'
-    sut = Moon(raw_data)
-    assert sut.p == Coordinate(11, 0, 11)
+    assert sut.p == Coordinate(x, y, z)
     assert sut.v == Coordinate(0, 0, 0)
