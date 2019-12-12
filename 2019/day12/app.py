1cbfad70b1f7c961f7a8bfac692bf4254a7be4fc
Peter Byfield <byfield554@gmail.com>
2019-12-12 18:37:56 +0000

    part 2! (#17)

--- 2019/day12/app.py
+++ 2019/day12/app.py
@@ -1,17 +1,41 @@
 import os
 import copy
+import numpy as np
 
 from moon import Moon
+from moonsperiodcalculator import MoonsPeriodCalculator
 
 
 def run_simulation(raw_data, n_steps):
     moons = [Moon(line) for line in raw_data]
     for i in range(n_steps):
-        for moon in moons:
-            moon.update_velocity(moons)
-        for moon in moons:
-            moon.update_position()
+        step(moons)
     return moons
+
+
+def find_period(raw_data):
+    moons = [Moon(line) for line in raw_data]
+    x_calculator = MoonsPeriodCalculator(lambda m: f'{m.p.x}:{m.v.x}', moons)
+    y_calculator = MoonsPeriodCalculator(lambda m: f'{m.p.y}:{m.v.y}', moons)
+    z_calculator = MoonsPeriodCalculator(lambda m: f'{m.p.z}:{m.v.z}', moons)
+    x_period = None
+    y_period = None
+    z_period = None
+    while True:
+        step(moons)
+        x_period = x_calculator.next(moons)
+        y_period = y_calculator.next(moons)
+        z_period = z_calculator.next(moons)
+        if (x_period is not None) and (y_period is not None) and (z_period is not None):
+            break
+    return np.lcm.reduce([x_period, y_period, z_period])
+
+
+def step(moons):
+    for moon in moons:
+        moon.update_velocity(moons)
+    for moon in moons:
+        moon.update_position()
 
 
 if __name__ == '__main__':
@@ -23,4 +47,5 @@ if __name__ == '__main__':
         moons = run_simulation(raw_data, 1000)
         print(f'Total energy = {sum([m.energy() for m in moons])}')
         print('Part 2')
-        print(f'')
+        period = find_period(raw_data)
+        print(f'Period = {period}')
