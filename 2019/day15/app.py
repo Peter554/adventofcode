24b4c61264b6adb27ab24dc6e6a35ed02e3fd523
Peter <byfield554@gmail.com>
2019-12-15 12:17:30 +0100

    wip

--- 2019/day15/app.py
+++ 2019/day15/app.py
@@ -1,7 +1,6 @@
 import os
 import random
 import asyncio
-import matplotlib.pyplot as plt
 
 from intcode import IntCode
 
@@ -36,6 +35,23 @@ async def find_oxygen(raw_code):
     return walls, oxygen, visited
 
 
+def get_minutes(walls, oxygen):
+    s = set()
+    s.add(oxygen)
+    minutes = 0
+    while True:
+        start_len = len(s)
+        for p in s.copy():
+            candindates = get_cantidates(p)
+            for c in candindates:
+                if not (c in walls):
+                    s.add(c)
+        if len(s) == start_len:
+            break
+        minutes += 1
+    return minutes
+
+
 def update_position(start, choice):
     if choice == 1:
         return (start[0] + 1, start[1])
@@ -49,6 +65,10 @@ def update_position(start, choice):
         raise Exception(f'Choice {choice} not supported')
 
 
+def get_cantidates(p):
+    return ((p[0]+1, p[1]), (p[0]-1, p[1]), (p[0], p[1]+1), (p[0], p[1] - 1))
+
+
 async def main():
     this_dir = os.path.dirname(os.path.abspath(__file__))
     input_path = os.path.join(this_dir, 'input.txt')
@@ -58,6 +78,8 @@ async def main():
         walls, oxygen, visited = await find_oxygen(raw_code)
         print(f'Steps = {len(visited)}')
         print('Part 2')
+        minutes = get_minutes(walls, oxygen)
+        print(f'Minutes = {minutes}')
 
 if __name__ == "__main__":
     asyncio.run(main())
