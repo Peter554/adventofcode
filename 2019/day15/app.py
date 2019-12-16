810087e52df0023170c6627d94d63ae8bf97bc08
Peter Byfield <byfield554@gmail.com>
2019-12-16 17:55:40 +0100

    Day15 (#18)

---
+++ 2019/day15/app.py
@@ -0,0 +1,64 @@
+import os
+import random
+import asyncio
+
+from intcode import IntCode
+
+
+async def find_oxygen(raw_code):
+    computer = IntCode(raw_code, asyncio.Queue())
+    computer_task = asyncio.create_task(computer.run())
+    position = (0, 0)
+    oxygen = None
+    path = set()
+    _walls = set()
+    while True:
+        choice = random.choice([1, 2, 3, 4])
+        candidate = update_position(position, choice)
+        if candidate in _walls:
+            continue
+        await computer.input_queue.put(choice)
+        output = await computer.output_queue.get()
+        assert_valid_output(output)
+        if output == 0:
+            _walls.add(candidate)
+        else:
+            if candidate in path:
+                path.remove(position)
+            position = candidate
+            path.add(position)
+        if output == 2:
+            oxygen = position
+            break
+    return oxygen, path
+
+
+def update_position(start, choice):
+    if choice == 1:
+        return (start[0] + 1, start[1])
+    elif choice == 2:
+        return (start[0] - 1, start[1])
+    elif choice == 3:
+        return (start[0], start[1] - 1)
+    elif choice == 4:
+        return (start[0], start[1] + 1)
+    else:
+        raise Exception(f'Choice {choice} not supported')
+
+
+def assert_valid_output(output):
+    if not (output in [0, 1, 2]):
+        raise Exception(f'Unexpected output {output}')
+
+
+async def main():
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    input_path = os.path.join(this_dir, 'input.txt')
+    with open(input_path) as f:
+        raw_code = f.readline()
+        print('Part 1')
+        oxygen, path = await find_oxygen(raw_code)
+        print(f'Steps = {len(path)}')
+
+if __name__ == "__main__":
+    asyncio.run(main())
