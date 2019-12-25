6ecfdb8b2e6ac4a652426a5ce7a8191f77771ce9
Peter <byfield554@gmail.com>
2019-12-25 18:19:47 +0000

    WIP on day 13

--- 2019/day13/app.py
+++ 2019/day13/app.py
@@ -6,20 +6,45 @@ from intcode import IntCode
 from consoledrawer import ConsoleDrawer
 
 
+class ArcadeController():
+    def __init__(self, arcade):
+        self._arcade = arcade
+
+    async def get(self):
+        inverted = {v: k for k, v in self._arcade.state.items()}
+        if 3 in inverted:
+            print(inverted)
+        if 3 in inverted and 4 in inverted:
+            paddle = inverted[3]
+            ball = inverted[4]
+            if paddle[0] == ball[0]:
+                return 0
+            elif paddle[0] < ball[0]:
+                return -1
+            elif paddle[0] > ball[0]:
+                return +1
+        return 0
+
+
 class Arcade():
     def __init__(self, raw_code):
         raw_code = '2' + raw_code[1:]
-        self._computer = IntCode(raw_code, asyncio.Queue())
+        contoller = ArcadeController(self)
+        self._computer = IntCode(raw_code, contoller)
+        self._d = {}
+        self._score = None
+
+    @property
+    def state(self):
+        return self._d
 
     async def run(self):
         self._stopped = False
         self._d = {}
         self._score = None
-        self._ball_history = []
         tasks = []
         tasks.append(asyncio.create_task(self._computer.run()))
         tasks.append(asyncio.create_task(self._draw()))
-        tasks.append(asyncio.create_task(self._receive_input()))
         await asyncio.gather(*tasks)
 
     async def stop(self):
@@ -28,6 +53,7 @@ class Arcade():
     async def _draw(self):
         while not self._stopped:
             try:
+                await asyncio.sleep(0.01)
                 x = await asyncio.wait_for(self._computer.output_queue.get(), 1)
                 y = await asyncio.wait_for(self._computer.output_queue.get(), 1)
                 tile_id = await asyncio.wait_for(self._computer.output_queue.get(), 1)
@@ -37,22 +63,14 @@ class Arcade():
                 self._score = tile_id
             else:
                 self._d[(x, y)] = tile_id
-                if tile_id == 4:
-                    self._ball_history.append((x, y))
             draw_arcade(self._d, self._score)
-
-    async def _receive_input(self):
-        while not self._stopped:
-            choice = random.choice([0, 1, -1])
-            self._computer.input_queue.put_nowait(choice)
-            await asyncio.sleep(0.01)
 
 
 def draw_arcade(arcade, score):
     key = {0: ' ', 1: '#', 2: '£', 3: '=', 4: 'o'}
-    drawer = ConsoleDrawer(key)
-    drawer.draw(arcade)
-    print(f'Score = {score}')
+    # drawer = ConsoleDrawer(key)
+    # drawer.draw(arcade)
+    # print(f'Score = {score}')
 
 
 async def main():
