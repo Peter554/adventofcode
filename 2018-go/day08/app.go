9421d0a91a252ce78074fb6ce6767e47697b26ac
Peter <byfield554@gmail.com>
2020-02-13 14:56:42 +0100

    init

---
+++ 2018-go/day08/app.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/peter554/adventofcode/2018-go/common"
+)
+
+func main() {
+	lines := common.Readlines("./input.txt")
+	rawData := lines[0]
+	fmt.Println(rawData)
+}
