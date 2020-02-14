e7c0fe1a2715cff33cbd0e9c9cbdaea9e6403d91
Peter <byfield554@gmail.com>
2020-02-14 13:17:43 +0100

    done

--- 2018-go/day10/app.go
+++ 2018-go/day10/app.go
@@ -1,12 +1,26 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/peter554/adventofcode/2018-go/common"
 )
 
 func main() {
 	lines := common.Readlines("./input.txt")
-	fmt.Println(lines)
+	cloud := NewPointCloud(lines)
+
+	collided := false
+	for {
+		cloud.Step()
+		extent := cloud.GetExtent()
+		xrange := extent.xmax - extent.xmin
+		yrange := extent.ymax - extent.ymin
+		if xrange < 100 && yrange < 15 {
+			collided = true
+			cloud.Print()
+			continue
+		}
+		if collided {
+			break
+		}
+	}
 }
