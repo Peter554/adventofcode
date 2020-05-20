db01bf84b24013e5efcc942a609feee62aa56ca0
Peter Byfield <byfield554@gmail.com>
2020-05-20 18:12:19 +0200

    WIP

--- 2018-go/day11/main.go
+++ 2018-go/day11/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func getHundreds(x int) int {
-	return 9
+	f := float64(x)
+	h := math.Floor(f/100) - math.Floor(f/1000)*10
+	return int(h)
 }
 
-func getFuelLevel(x int, y int, offset int) int {
+func getCellFuelLevel(x int, y int, offset int) int {
 	o := x + 10
 	o = o * y
 	o += offset
@@ -16,7 +21,32 @@ func getFuelLevel(x int, y int, offset int) int {
 	return o
 }
 
+func getGridFuelLevel(x int, y int, offset int) int {
+	t := 0
+	for i := x; i < x+3; i++ {
+		for j := y; j < y+3; j++ {
+			t += getCellFuelLevel(i, j, offset)
+		}
+	}
+	return t
+}
+
 func main() {
-	fuelLevel := getFuelLevel(3, 5, 8)
-	fmt.Println(fuelLevel)
+	offset := 9221
+	maxX := 1
+	maxY := 1
+	maxFuel := getGridFuelLevel(maxX, maxY, offset)
+
+	for i := 1; i <= 300; i++ {
+		for j := 1; j <= 300; j++ {
+			fuel := getGridFuelLevel(i, j, offset)
+			if fuel > maxFuel {
+				maxX = i
+				maxY = j
+				maxFuel = fuel
+			}
+		}
+	}
+
+	fmt.Printf("(%d, %d)\n", maxX, maxY)
 }
