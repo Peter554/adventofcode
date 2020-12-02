b69686f385b6011ea42a9712f4550ce23b2b7062
Peter Byfield <byfield554@gmail.com>
2020-12-02 08:37:32 +0100

    2020 day 2

---
+++ 2020/day02/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("./input.txt")
+	check(err)
+	defer file.Close()
+
+	lines := []string{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+
+	records := []record{}
+	for _, line := range lines {
+		records = append(records, parseLine(line))
+	}
+
+	validPasswordsCount := 0
+	for _, r := range records {
+		letterCount := 0
+		for _, letter := range r.Password {
+			if letter == r.Letter {
+				letterCount++
+			}
+		}
+		if letterCount >= r.X && letterCount <= r.Y {
+			validPasswordsCount++
+		}
+	}
+	fmt.Println("# of valid passwords is", validPasswordsCount)
+
+	validPasswordsCount = 0
+	for _, r := range records {
+		isValid := false
+		for idx, letter := range r.Password {
+			if (idx+1 == r.X || idx+1 == r.Y) && letter == r.Letter {
+				isValid = !isValid
+			}
+		}
+		if isValid {
+			validPasswordsCount++
+		}
+	}
+	fmt.Println("# of valid passwords is", validPasswordsCount)
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type record struct {
+	X        int
+	Y        int
+	Letter   rune
+	Password string
+}
+
+func parseLine(line string) record {
+	re := regexp.MustCompile(`(\d+)-(\d+)\s([a-z]):\s([a-z]+)`)
+	match := re.FindStringSubmatch(line)
+	if match == nil {
+		panic("Line did not match regex")
+	}
+	x, _ := strconv.Atoi(match[1])
+	y, _ := strconv.Atoi(match[2])
+	return record{
+		X:        x,
+		Y:        y,
+		Letter:   rune(match[3][0]),
+		Password: match[4],
+	}
+}
