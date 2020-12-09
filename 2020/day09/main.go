270878ab07fe1b61fd45ce598f2586221b4f6c5e
Peter Byfield <byfield554@gmail.com>
2020-12-09 08:14:50 +0100

    2020 day 9

---
+++ 2020/day09/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/peter554/adventofcode/2020/lib"
+)
+
+func main() {
+	lines := lib.ReadInput()
+	ints := asInts(lines)
+
+	invalidNumber := firstInvalidNumber(ints)
+	fmt.Println("Part 1 =", invalidNumber)
+
+	block := findContiguousBlock(ints, func(block []int) bool {
+		return sumInts(block) == invalidNumber
+	})
+	min, max := minMaxInts(block)
+	fmt.Println("Part 2 =", min+max)
+}
+
+func asInts(lines []string) []int {
+	ints := []int{}
+	for _, line := range lines {
+		i, err := strconv.Atoi(line)
+		lib.Check(err)
+		ints = append(ints, i)
+	}
+	return ints
+}
+
+func firstInvalidNumber(ints []int) int {
+	for idx, i := range ints[25:] {
+		valid := false
+
+		m := map[int]int{}
+		for _, j := range ints[idx : idx+25] {
+			if _, exists := m[j]; exists {
+				m[j]++
+			} else {
+				m[j] = 1
+			}
+		}
+
+		for k, v := range m {
+			if k*2 == i && v >= 2 {
+				valid = true
+			}
+			if _, exists := m[i-k]; exists {
+				valid = true
+			}
+		}
+
+		if !valid {
+			return i
+		}
+	}
+	panic("Invalid number not found")
+}
+
+func findContiguousBlock(ints []int, match func(block []int) bool) []int {
+	for i := 0; i < len(ints); i++ {
+		for j := i + 1; j < len(ints); j++ {
+			block := ints[i:j]
+			if match(block) {
+				return block
+			}
+		}
+	}
+	panic("Contiguous block not found")
+}
+
+func sumInts(ints []int) int {
+	o := 0
+	for _, i := range ints {
+		o += i
+	}
+	return o
+}
+
+func minMaxInts(ints []int) (min, max int) {
+	min, max = ints[0], ints[0]
+	for _, i := range ints {
+		if i < min {
+			min = i
+		}
+		if i > max {
+			max = i
+		}
+	}
+	return
+}
