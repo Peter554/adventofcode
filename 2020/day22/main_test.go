b227e19c73515eed6fbc6557d8424bc1322cec76
Peter Byfield <byfield554@gmail.com>
2020-12-22 15:17:15 +0100

    2020 day 22 (#58)

---
+++ 2020/day22/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/peter554/adventofcode/2020/lib"
+)
+
+func TestPart1(t *testing.T) {
+	lib.UseInput("sample")
+	lines := lib.ReadInput()
+	lib.Expect(t, 306, Part1(lines))
+
+	lib.UseInput("input")
+	lines = lib.ReadInput()
+	lib.Expect(t, 33772, Part1(lines))
+}
+
+func TestPart2(t *testing.T) {
+	lib.UseInput("sample")
+	lines := lib.ReadInput()
+	lib.Expect(t, 291, Part2(lines))
+
+	lib.UseInput("input")
+	lines = lib.ReadInput()
+	lib.Expect(t, 35070, Part2(lines))
+}
