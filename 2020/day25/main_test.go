ad4f0020cc1b1a9896cbee610b90977a7083db26
Peter Byfield <byfield554@gmail.com>
2020-12-25 11:14:35 +0100

    2020 day 25

---
+++ 2020/day25/main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/peter554/adventofcode/2020/lib"
+)
+
+func TestPart1(t *testing.T) {
+	lib.Expect(t, 5414549, Part1())
+}
+
+func Test_findLoopSize(t *testing.T) {
+	lib.Expect(t, 8, findLoopSize(5764801))
+	lib.Expect(t, 11, findLoopSize(17807724))
+}
