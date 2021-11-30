d301034af84605d2472c91ee7e92ad3b8592f814
Peter Byfield <byfield554@gmail.com>
2021-11-30 23:18:02 +0100

    2021 prep (#62)

---
+++ 2021/day00/solution.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+
+# 2020 day 1
+def part_1(file_path) -> Optional[int]:
+    with open(file_path, "r") as f:
+        numbers = [int(line) for line in f.readlines()]
+    for i, n in enumerate(numbers):
+        for j, m in enumerate(numbers):
+            if i == j:
+                continue
+            if n + m == 2020:
+                return n * m
+    return None
---
+++ 2021/day00/test_solution.py
@@ -0,0 +1,6 @@
+from day00 import solution
+
+
+def test_part_1():
+    assert solution.part_1("day00/sample") == 514579
+    assert solution.part_1("day00/input") == 494475
