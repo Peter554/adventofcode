02620c0d0949b4050321144cc4b4d40f9f738e4e
Peter Byfield <byfield554@gmail.com>
2021-12-02 08:06:11 +0100

    Refactor 2021 day 1

--- 2021/day01/solution.py
+++ 2021/day01/solution.py
@@ -1,13 +1,11 @@
 def part_1(file_path) -> int:
     with open(file_path, "r") as f:
         numbers = [int(line) for line in f]
-    return sum([numbers[i] > numbers[i - 1] for i in range(1, len(numbers))])
+    return sum([numbers[i] < numbers[i + 1] for i in range(len(numbers) - 1)])
 
 
 def part_2(file_path) -> int:
     with open(file_path, "r") as f:
         numbers = [int(line) for line in f]
-    numbers = [
-        numbers[i] + numbers[i - 1] + numbers[i - 2] for i in range(2, len(numbers))
-    ]
-    return sum([numbers[i] > numbers[i - 1] for i in range(1, len(numbers))])
+    numbers = [sum(numbers[i : i + 3]) for i in range(len(numbers) - 2)]
+    return sum([numbers[i] < numbers[i + 1] for i in range(len(numbers) - 1)])
