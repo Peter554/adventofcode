2cae6adc04ff9abcbe69970f392f035757117c21
Peter Byfield <byfield554@gmail.com>
2021-12-02 08:00:59 +0100

    2021 day 2 (#65)

---
+++ 2021/day02/solution.py
@@ -0,0 +1,29 @@
+def part_1(file_path) -> int:
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+    x, depth = 0, 0
+    for line in lines:
+        cmd, n = line.split()
+        if cmd == "forward":
+            x += int(n)
+        elif cmd == "down":
+            depth += int(n)
+        elif cmd == "up":
+            depth -= int(n)
+    return x * depth
+
+
+def part_2(file_path) -> int:
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+    x, depth, aim = 0, 0, 0
+    for line in lines:
+        cmd, n = line.split()
+        if cmd == "forward":
+            x += int(n)
+            depth += aim * int(n)
+        elif cmd == "down":
+            aim += int(n)
+        elif cmd == "up":
+            aim -= int(n)
+    return x * depth
