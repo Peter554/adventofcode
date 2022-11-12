39954e979497ac28cee52835b53b4679deb41ba2
Peter Byfield <byfield554@gmail.com>
2022-11-12 22:52:21 +0100

    maven init

---
+++ 2015/aoc2015java/src/main/java/aoc2015/App.java
@@ -0,0 +1,13 @@
+package aoc2015;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
