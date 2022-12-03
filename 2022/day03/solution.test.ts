11dec629a92fafbac2ca27dbe36196a91b6da319
Peter Byfield <byfield554@gmail.com>
2022-12-03 06:53:01 +0100

    Init day 3 TypeScript

---
+++ 2022/day03/solution.test.ts
@@ -0,0 +1,7 @@
+import { part1 } from "./solution";
+
+describe("solution", () => {
+  it("solves part1", () => {
+    expect(part1()).toEqual(42);
+  });
+});
---
+++ 2022/day03/solution.ts
@@ -0,0 +1,3 @@
+export function part1() {
+  return 42;
+}
