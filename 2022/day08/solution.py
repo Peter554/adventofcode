555b540852329fe75b62b58896e811ec20669f00
Peter Byfield <byfield554@gmail.com>
2022-12-08 14:12:02 +0100

    Improve 2022 day 8

--- 2022/day08/solution.py
+++ 2022/day08/solution.py
@@ -19,43 +19,20 @@ def part_1(file_path: str) -> int:
             for idx_y, line in enumerate(f.readlines())
             for idx_x, char in enumerate(line.strip())
         }
-    max_idx_x, max_idx_y = max([dataclasses.astuple(tree) for tree in trees.keys()])
 
-    directions = [
-        # from left
-        (
-            [Point2D(0, idx_y) for idx_y in range(max_idx_y + 1)],
-            Point2D(1, 0),
-        ),
-        # from right
-        (
-            [Point2D(max_idx_x, idx_y) for idx_y in range(max_idx_y + 1)],
-            Point2D(-1, 0),
-        ),
-        # from top
-        (
-            [Point2D(idx_x, 0) for idx_x in range(max_idx_x + 1)],
-            Point2D(0, 1),
-        ),
-        # from bottom
-        (
-            [Point2D(idx_x, max_idx_y) for idx_x in range(max_idx_x + 1)],
-            Point2D(0, -1),
-        ),
-    ]
-
-    trees_visible = set()
-    for starting_trees, delta in directions:
-        for starting_tree in starting_trees:
-            tree = starting_tree
-            trees_visible.add(tree)
-            max_tree_height = trees[tree]
-            while (next_tree := tree + delta) in trees:
-                if trees[next_tree] > max_tree_height:
-                    trees_visible.add(next_tree)
-                    max_tree_height = trees[next_tree]
+    visible_trees = set()
+    for this_tree in trees:
+        for delta in [Point2D(1, 0), Point2D(-1, 0), Point2D(0, 1), Point2D(0, -1)]:
+            tree = this_tree
+            while (next_tree := tree + delta) in trees and trees[next_tree] < trees[
+                this_tree
+            ]:
                 tree = next_tree
-    return len(trees_visible)
+            if next_tree not in trees:
+                # we reached the edge of the forest 🎋
+                visible_trees.add(this_tree)
+                break
+    return len(visible_trees)
 
 
 def part_2(file_path: str) -> int:
@@ -67,17 +44,16 @@ def part_2(file_path: str) -> int:
         }
 
     treehouse_scores: dict[Point2D, int] = {}
-    for treehouse in trees:
-        treehouse_height = trees[treehouse]
+    for this_tree in trees:
         treehouse_score = 1
         for delta in [Point2D(1, 0), Point2D(-1, 0), Point2D(0, 1), Point2D(0, -1)]:
-            tree = treehouse
-            treehouse_direction_score = 0
+            tree = this_tree
+            direction_score = 0
             while (next_tree := tree + delta) in trees:
-                treehouse_direction_score += 1
-                if trees[next_tree] >= treehouse_height:
+                direction_score += 1
+                if trees[next_tree] >= trees[this_tree]:
                     break
                 tree = next_tree
-            treehouse_score *= treehouse_direction_score
-        treehouse_scores[treehouse] = treehouse_score
+            treehouse_score *= direction_score
+        treehouse_scores[this_tree] = treehouse_score
     return max(treehouse_scores.values())
