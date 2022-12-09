e688468583e8feb79417d9d9a53929590fef6be0
Peter Byfield <byfield554@gmail.com>
2022-12-09 09:48:23 +0100

    Improve 2022 day 9 more

--- 2022/day09/solution.py
+++ 2022/day09/solution.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import dataclasses
+from typing import Literal
 
 
 @dataclasses.dataclass(frozen=True)
@@ -18,53 +19,64 @@ class Point2D:
         return max(abs(self.x - other.x), abs(self.y - other.y)) <= 1
 
 
-DIRECTION_MAPPING = {
-    "R": Point2D(1, 0),
-    "L": Point2D(-1, 0),
-    "U": Point2D(0, 1),
-    "D": Point2D(0, -1),
-}
+Direction = Literal["U", "D", "L", "R"]
+
+
+def parse_direction(s: str) -> Direction:
+    mapping: dict[str, Direction] = {"U": "U", "D": "D", "L": "L", "R": "R"}
+    return mapping[s]
+
+
+class Rope:
+    _DIRECTION_DELTA_MAPPING: dict[Direction, Point2D] = {
+        "U": Point2D(0, 1),
+        "D": Point2D(0, -1),
+        "L": Point2D(-1, 0),
+        "R": Point2D(1, 0),
+    }
+
+    def __init__(self, length: int):
+        self._state = [Point2D(0, 0) for _ in range(length)]
+
+    @property
+    def state(self) -> tuple[Point2D, ...]:
+        return tuple(self._state)
+
+    def evolve(self, direction: Direction) -> None:
+        next_state = [self._state[0] + self._DIRECTION_DELTA_MAPPING[direction]]
+        for idx in range(1, len(self._state)):
+            delta = self._get_delta(self._state[idx], next_state[idx - 1])
+            next_state.append(self._state[idx] + delta)
+        self._state = next_state
+
+    @staticmethod
+    def _get_delta(tail: Point2D, head: Point2D) -> Point2D:
+        if tail.touching(head):
+            return Point2D(0, 0)
+        # same row/column: move horizontal
+        # else: move diagonal
+        return Point2D(
+            (head.x - tail.x) // (abs(head.x - tail.x) or 1),
+            (head.y - tail.y) // (abs(head.y - tail.y) or 1),
+        )
+
+
+def solve_puzzle(file_path: str, rope_length: int) -> int:
+    with open(file_path) as f:
+        instructions = [line.strip() for line in f.readlines()]
+    rope = Rope(rope_length)
+    rope_tail_history = {rope.state[-1]}
+    for instruction in instructions:
+        direction, count = instruction.split(" ")
+        for _ in range(int(count)):
+            rope.evolve(parse_direction(direction))
+            rope_tail_history.add(rope.state[-1])
+    return len(rope_tail_history)
 
 
 def part_1(file_path: str) -> int:
-    with open(file_path) as f:
-        instructions = [line.strip() for line in f.readlines()]
-
-    head, tail = Point2D(0, 0), Point2D(0, 0)
-    tail_history = {tail}
-    for instruction in instructions:
-        direction, count = instruction.split(" ")
-        for _ in range(int(count)):
-            t = head
-            head = head + DIRECTION_MAPPING[direction]
-            if not tail.touching(head):
-                tail = t
-                tail_history.add(tail)
-    return len(tail_history)
-
-
-def get_delta(p1: Point2D, p2: Point2D) -> Point2D:
-    if p1.touching(p2):
-        return Point2D(0, 0)
-    return Point2D(
-        (p2.x - p1.x) // (abs(p2.x - p1.x) or 1),
-        (p2.y - p1.y) // (abs(p2.y - p1.y) or 1),
-    )
+    return solve_puzzle(file_path, 2)
 
 
 def part_2(file_path: str) -> int:
-    with open(file_path) as f:
-        instructions = [line.strip() for line in f.readlines()]
-
-    rope = [Point2D(0, 0) for _ in range(10)]
-    tail_history = {rope[-1]}
-    for instruction in instructions:
-        direction, count = instruction.split(" ")
-        for _ in range(int(count)):
-            next_rope = [rope[0] + DIRECTION_MAPPING[direction]]
-            for idx in range(1, 10):
-                delta = get_delta(rope[idx], next_rope[idx - 1])
-                next_rope.append(rope[idx] + delta)
-            rope = next_rope
-            tail_history.add(rope[-1])
-    return len(tail_history)
+    return solve_puzzle(file_path, 10)
