78f48df93bfa56f4a0def8cbcc0ff92f9132c5ed
Peter Byfield <byfield554@gmail.com>
2022-12-14 17:07:14 +0100

    2022 day 14 draw cave

--- 2022/day14/solution.py
+++ 2022/day14/solution.py
@@ -1,5 +1,8 @@
 import enum
 
+import numpy
+from PIL import Image
+
 
 class CaveTexture(enum.Enum):
     EMPTY = enum.auto()
@@ -82,3 +85,52 @@ def part_2(file_path: str) -> int:
     while drop_sand(cave) and cave[0][500] == CaveTexture.EMPTY:
         ...
     return count_sand(cave)
+
+
+RGB = tuple[int, int, int]
+
+
+def create_cave_image(
+    cave: Cave,
+    color_map: dict[CaveTexture, RGB] | None = None,
+    size_factor: int = 8,
+):
+    color_map = color_map or {
+        CaveTexture.EMPTY: (211, 211, 211),
+        CaveTexture.ROCK: (128, 128, 128),
+        CaveTexture.SAND: (194, 178, 128),
+    }
+
+    x_min = min(
+        x
+        for cave_row in cave
+        for x, cave_texture in enumerate(cave_row)
+        if cave_texture == CaveTexture.SAND
+    )
+    x_max = max(
+        x
+        for cave_row in cave
+        for x, cave_texture in enumerate(cave_row)
+        if cave_texture == CaveTexture.SAND
+    )
+    img_array = numpy.zeros(
+        dtype=numpy.uint8,
+        shape=(size_factor * len(cave), size_factor * (x_max - x_min + 1), 3),
+    )
+    for y, cave_row in enumerate(cave):
+        for x, cave_texture in enumerate(cave_row[x_min : x_max + 1]):
+            img_array[
+                y * size_factor : (y + 1) * size_factor,
+                x * size_factor : (x + 1) * size_factor,
+            ] = color_map[cave_texture]
+    return Image.fromarray(img_array, "RGB")
+
+
+if __name__ == "__main__":
+    with open("day14/input") as f:
+        raw_rocks = [line.strip() for line in f.readlines()]
+    cave = initialize_cave(raw_rocks, 2)
+    while drop_sand(cave) and cave[0][500] == CaveTexture.EMPTY:
+        ...
+    img = create_cave_image(cave)
+    img.save("day14/cave.png")
