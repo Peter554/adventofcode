4e6f36d4ce04578c40fbcac280256b32cfa79f18
Peter Byfield <byfield554@gmail.com>
2022-12-20 14:08:36 +0100

    part 2

--- 2022/day20/solution.py
+++ 2022/day20/solution.py
@@ -10,8 +10,8 @@ class Node:
     right: Node | None
 
 
-def parse_nodes(lines: list[str]) -> list[Node]:
-    nodes = [Node(int(line), None, None) for line in lines]
+def parse_nodes(lines: list[str], decryption_key: int) -> list[Node]:
+    nodes = [Node(int(line) * decryption_key, None, None) for line in lines]
     nodes[0].left = nodes[-1]
     nodes[-1].right = nodes[0]
     for idx, l_node in enumerate(nodes[:-1]):
@@ -21,9 +21,15 @@ def parse_nodes(lines: list[str]) -> list[Node]:
     return nodes
 
 
+def normalize_node_value(value: int, n_nodes: int) -> int:
+    return value % (n_nodes - 1)
+
+
 def remove_node(node: Node):
     right = node.right
     left = node.left
+    assert right is not None
+    assert left is not None
     right.left = left
     left.right = right
     node.right = None
@@ -32,45 +38,38 @@ def remove_node(node: Node):
 
 def insert_node(node: Node, right: Node):
     left = right.left
+    assert left is not None
     node.left = left
     node.right = right
     right.left = node
     left.right = node
 
 
-def print_nodes(root: Node):
-    nodes = [root]
-    while nodes[-1].right not in nodes:
-        nodes.append(nodes[-1].right)
-    nodes.append(nodes[-1].right)
-    s = " -> ".join([f"{node.value}" for node in nodes])
-    print(s)
+def mix_nodes(nodes: list[Node]):
+    for node in nodes:
+        right = node.right
+        remove_node(node)
+        for _ in range(normalize_node_value(node.value, len(nodes))):
+            assert right is not None
+            right = right.right
+        assert right is not None
+        insert_node(node, right)
 
 
 def part_1(file_path: str) -> int:
     with open(file_path) as f:
-        nodes = parse_nodes(f.readlines())
+        nodes = parse_nodes(f.readlines(), 1)
 
     zero_nodes = [node for node in nodes if node.value == 0]
     assert len(zero_nodes) == 1
     zero_node = zero_nodes[0]
 
-    for node in nodes:
-        if node.value == 0:
-            continue
-        right = node.right
-        remove_node(node)
-        if node.value >= 0:
-            for _ in range(node.value):
-                right = right.right
-        else:
-            for _ in range(abs(node.value)):
-                right = right.left
-        insert_node(node, right)
+    mix_nodes(nodes)
 
     grove_coordinates: list[int] = []
     node = zero_node
     for i in range(1, 3001):
+        assert node.right is not None
         node = node.right
         if i % 1000 == 0:
             grove_coordinates.append(node.value)
@@ -78,4 +77,21 @@ def part_1(file_path: str) -> int:
 
 
 def part_2(file_path: str) -> int:
-    return 1
+    with open(file_path) as f:
+        nodes = parse_nodes(f.readlines(), 811589153)
+
+    zero_nodes = [node for node in nodes if node.value == 0]
+    assert len(zero_nodes) == 1
+    zero_node = zero_nodes[0]
+
+    for _ in range(10):
+        mix_nodes(nodes)
+
+    grove_coordinates: list[int] = []
+    node = zero_node
+    for i in range(1, 3001):
+        assert node.right is not None
+        node = node.right
+        if i % 1000 == 0:
+            grove_coordinates.append(node.value)
+    return sum(grove_coordinates)
--- 2022/day20/test_solution.py
+++ 2022/day20/test_solution.py
@@ -7,5 +7,5 @@ def test_part_1():
 
 
 def test_part_2():
-    assert solution.part_2("day20/sample") == 1
-    assert solution.part_2("day20/input") == 1
+    assert solution.part_2("day20/sample") == 1623178306
+    # assert solution.part_2("day20/input") == 7768531372516
