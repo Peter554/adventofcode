2592c8eb1e36dcc96276f1428b91e75ecf6c867d
Peter Byfield <byfield554@gmail.com>
2022-12-22 11:02:25 +0100

    parse maps

--- 2022/day22/solution.py
+++ 2022/day22/solution.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import enum
 import re
 
+from common.point2d import Point2D
+
 ROUTE_PATH = "day22/route"
 MAPS_PATH = "day22/maps"
 
@@ -30,6 +32,21 @@ def parse_route() -> list[int | Turn]:
             route.append(int(t))
             s = s[len(t) :]
     return route
+
+
+def parse_maps() -> dict[int, set[Point2D]]:
+    with open(MAPS_PATH) as f:
+        raw_maps = f.read().split("\n\n")
+    maps: dict[int, set[Point2D]] = {}
+    for idx, raw_map in enumerate(raw_maps):
+        map_: set[Point2D] = set()
+        for y, line in enumerate(raw_map.split()):
+            for x, char in enumerate(line.strip()):
+                if char == "#":
+                    continue
+                map_.add(Point2D(x, y))
+        maps[idx] = map_
+    return maps
 
 
 def part_1() -> int:
--- 2022/day22/test_solution.py
+++ 2022/day22/test_solution.py
@@ -8,6 +8,14 @@ def test_parse_route():
     assert route[2] == 7
 
 
+def test_parse_maps():
+    maps = solution.parse_maps()
+    assert set(maps) == {0, 1, 2, 3, 4, 5}
+    assert solution.Point2D(0, 0) in maps[0]
+    assert solution.Point2D(2, 1) not in maps[0]
+    assert solution.Point2D(49, 49) in maps[0]
+
+
 def test_part_1():
     assert solution.part_1() == 1
 
