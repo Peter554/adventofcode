ba109f0e839693a0b1d83d7231a020ba4c554c9a
Peter Byfield <byfield554@gmail.com>
2022-12-25 17:44:22 +0100

    add p1 answer

--- 2022/day17/test_solution_part_1.py
+++ 2022/day17/test_solution_part_1.py
@@ -3,4 +3,4 @@ from day17 import solution_part_1
 
 def test_solve():
     assert solution_part_1.solve("day17/sample") == 3068
-    assert solution_part_1.solve("day17/input") == 1
+    assert solution_part_1.solve("day17/input") == 3069
