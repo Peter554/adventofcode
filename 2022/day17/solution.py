c68e4c8190b20301570ff44e3b35adfef4c9e221
Peter Byfield <byfield554@gmail.com>
2023-01-09 21:27:38 +0100

    2022 day 17 part 2

--- 2022/day17/solution.py
+++ 2022/day17/solution.py
@@ -1,5 +1,3 @@
-import itertools
-
 from common.point2d import Point2D
 
 rock_templates = [
@@ -28,35 +26,37 @@ def move_rock(rock: frozenset[Point2D], delta: Point2D) -> frozenset[Point2D]:
     return frozenset(p + delta for p in rock)
 
 
-def print_rubble(rubble: set[Point2D], max_y: int):
+def rubble_signature(rubble: set[Point2D], rubble_height: int) -> str:
     s = ""
-    for y in range(max_y, 0, -1):
+    for y in range(rubble_height, max(rubble_height - 50, 0), -1):
         for x in range(1, 8):
-            if Point2D(x, y) in rubble:
-                s += "#"
-            else:
-                s += "."
-        s += "\n"
-    print(s)
+            s += "1" if Point2D(x, y) in rubble else "0"
+    return s
 
 
-def part_1(file_path: str) -> int:
+def solve(file_path: str, rocks_to_drop: int) -> int:
     with open(file_path) as f:
-        air_cycle = itertools.cycle(
+        air_cycle = [
             {
                 "<": Point2D(-1, 0),
                 ">": Point2D(1, 0),
             }[char]
             for char in f.readline().strip()
-        )
+        ]
 
-    rock_cycle = itertools.cycle(
+    rock_cycle = [
         frozenset(Point2D(x, y) for x, y in rock_template)
         for rock_template in rock_templates
-    )
+    ]
 
+    rock_idx, air_idx = 0, 0
+    remaining_rocks_to_drop = rocks_to_drop
     rubble: set[Point2D] = set()
     rubble_height = 0
+
+    # cycle
+    history: dict = {}
+    extra_rubble_height = 0
 
     def try_move_rock(
         rock: frozenset[Point2D], delta: Point2D
@@ -72,16 +72,37 @@ def part_1(file_path: str) -> int:
             return False, rock
         return True, moved_rock
 
-    for _ in range(2022):
-        rock = next(rock_cycle)
-        rock = move_rock(rock, Point2D(2, 3 + rubble_height))
+    while remaining_rocks_to_drop:
+        rocks_dropped = rocks_to_drop - remaining_rocks_to_drop
+        history_key = (rubble_signature(rubble, rubble_height), rock_idx, air_idx)
+        if history_key in history:
+            previous_rocks_dropped, previous_rubble_height = history[history_key]
+            delta_rocks_dropped = rocks_dropped - previous_rocks_dropped
+            delta_height = rubble_height - previous_rubble_height
+            n = remaining_rocks_to_drop // delta_rocks_dropped
+            extra_rubble_height += delta_height * n
+            remaining_rocks_to_drop -= delta_rocks_dropped * n
+        else:
+            history[history_key] = (rocks_dropped, rubble_height)
+
+        rock = move_rock(rock_cycle[rock_idx], Point2D(2, 3 + rubble_height))
+        rock_idx = (rock_idx + 1) % len(rock_cycle)
         while True:
-            _, rock = try_move_rock(rock, next(air_cycle))
+            _, rock = try_move_rock(rock, air_cycle[air_idx])
+            air_idx = (air_idx + 1) % len(air_cycle)
             moved, rock = try_move_rock(rock, Point2D(0, -1))
             if not moved:
                 rubble = rubble.union(rock)
                 rubble_height = max(p.y for p in rubble)
-                # print_rubble(rubble, rubble_height)
+                remaining_rocks_to_drop -= 1
                 break
 
-    return rubble_height
+    return rubble_height + extra_rubble_height
+
+
+def part_1(file_path: str) -> int:
+    return solve(file_path, 2022)
+
+
+def part_2(file_path: str) -> int:
+    return solve(file_path, 1_000_000_000_000)
--- 2022/day17/test_solution.py
+++ 2022/day17/test_solution.py
@@ -4,3 +4,8 @@ from day17 import solution
 def test_part_1():
     assert solution.part_1("day17/sample") == 3068
     assert solution.part_1("day17/input") == 3069
+
+
+def test_part_2():
+    assert solution.part_2("day17/sample") == 1514285714288
+    assert solution.part_2("day17/input") == 1523167155404
