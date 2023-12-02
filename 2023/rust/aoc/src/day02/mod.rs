99fbbebd009e4a106df2c4c293fb4907ca995f78
Peter Byfield <17687648+Peter554@users.noreply.github.com>
2023-12-02 19:26:26 +0100

    2023 day 2 (#82)

---
+++ 2023/rust/aoc/src/day02/mod.rs
@@ -0,0 +1,105 @@
+use anyhow::Result;
+use std::{cmp, fs, path::Path};
+
+pub fn part1(input_path: &Path) -> Result<i64> {
+    let input = fs::read_to_string(input_path)?;
+    let games = input.lines().map(parse_game).collect::<Vec<_>>();
+    Ok(games
+        .iter()
+        .filter(|g| {
+            g.hands
+                .iter()
+                .all(|h| h.red <= 12 && h.green <= 13 && h.blue <= 14)
+        })
+        .map(|g| g.id as i64)
+        .sum())
+}
+
+pub fn part2(input_path: &Path) -> Result<i64> {
+    let input = fs::read_to_string(input_path)?;
+    let games = input.lines().map(parse_game).collect::<Vec<_>>();
+    Ok(games
+        .iter()
+        .map(|g| {
+            g.hands
+                .iter()
+                .fold((0, 0, 0), |(mut red, mut green, mut blue), hand| {
+                    red = cmp::max(red, hand.red);
+                    green = cmp::max(green, hand.green);
+                    blue = cmp::max(blue, hand.blue);
+                    (red, green, blue)
+                })
+        })
+        .map(|(red, green, blue)| (red as i64, green as i64, blue as i64))
+        .map(|(red, green, blue)| red * green * blue)
+        .sum())
+}
+
+fn parse_game(raw_game: &str) -> Game {
+    let mut it = raw_game.split(':');
+    let id = it
+        .next()
+        .unwrap()
+        .strip_prefix("Game ")
+        .unwrap()
+        .parse::<u8>()
+        .unwrap();
+    let hands = it
+        .next()
+        .unwrap()
+        .split(';')
+        .map(|raw_hand| {
+            let mut hand = Hand {
+                red: 0,
+                green: 0,
+                blue: 0,
+            };
+            raw_hand.split(',').for_each(|raw_segment| {
+                let mut it = raw_segment.trim().split(' ');
+                let n = it.next().unwrap().parse::<u8>().unwrap();
+                match it.next().unwrap() {
+                    "red" => hand.red = n,
+                    "green" => hand.green = n,
+                    "blue" => hand.blue = n,
+                    _ => panic!(),
+                }
+            });
+            hand
+        })
+        .collect::<Vec<_>>();
+    Game { id, hands }
+}
+
+struct Game {
+    id: u8,
+    hands: Vec<Hand>,
+}
+
+struct Hand {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let input_path = Path::new("./src/day02/sample");
+        assert_eq!(part1(input_path).unwrap(), 8);
+
+        let input_path = Path::new("./src/day02/input");
+        assert_eq!(part1(input_path).unwrap(), 2149);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input_path = Path::new("./src/day02/sample");
+        assert_eq!(part2(input_path).unwrap(), 2286);
+
+        let input_path = Path::new("./src/day02/input");
+        assert_eq!(part2(input_path).unwrap(), 71274);
+    }
+}
