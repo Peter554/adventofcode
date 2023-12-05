31f061d9707b20dec4594cc009e314c44e7005a0
Peter Byfield <byfield554@gmail.com>
2023-12-05 11:53:34 +0100

    Optimize 2023 day 5

--- 2023/rust/aoc/src/day05/mod.rs
+++ 2023/rust/aoc/src/day05/mod.rs
@@ -7,30 +7,30 @@ pub fn part1(input_path: &Path) -> Result<i64> {
     Ok(input
         .seeds
         .into_iter()
-        .map(|seed| input.maps.iter().fold(seed, |value, map| map.apply(value)))
+        .map(|seed| seed_to_location(seed, &input.maps))
         .min()
         .unwrap() as i64)
 }
 
 pub fn part2(input_path: &Path) -> Result<i64> {
     let input = fs::read_to_string(input_path)?;
-    let input = {
-        let mut input = parse_input(&input);
-        input.seeds = input
-            .seeds
-            .as_slice()
-            .windows(2)
-            .step_by(2)
-            .flat_map(|window| (window[0]..window[0] + window[1]))
-            .collect();
-        input
-    };
-    Ok(input
+    let input = parse_input(&input);
+    let seed_windows = input
         .seeds
-        .into_iter()
-        .map(|seed| input.maps.iter().fold(seed, |value, map| map.apply(value)))
-        .min()
-        .unwrap() as i64)
+        .as_slice()
+        .windows(2)
+        .step_by(2)
+        .map(|window| (window[0], window[1]))
+        .collect::<Vec<_>>();
+    for location in 1.. {
+        let seed = location_to_seed(location, &input.maps);
+        if seed_windows.iter().any(|(window_start, window_size)| {
+            seed >= *window_start && seed < window_start + window_size
+        }) {
+            return Ok(location as i64);
+        }
+    }
+    unreachable!()
 }
 
 fn parse_input(input: &str) -> Input {
@@ -66,6 +66,16 @@ fn parse_input(input: &str) -> Input {
     Input { seeds, maps }
 }
 
+fn seed_to_location(seed: u64, maps: &[Map]) -> u64 {
+    maps.iter().fold(seed, |value, map| map.apply(value))
+}
+
+fn location_to_seed(location: u64, maps: &[Map]) -> u64 {
+    maps.iter()
+        .rev()
+        .fold(location, |value, map| map.apply_reverse(value))
+}
+
 #[derive(Debug)]
 struct Input {
     seeds: Vec<u64>,
@@ -90,6 +100,17 @@ impl Map {
             if value >= rule.source_range_start && value < rule.source_range_start + rule.range_size
             {
                 return rule.destination_range_start + (value - rule.source_range_start);
+            }
+        }
+        value
+    }
+
+    fn apply_reverse(&self, value: u64) -> u64 {
+        for rule in self.rules.iter() {
+            if value >= rule.destination_range_start
+                && value < rule.destination_range_start + rule.range_size
+            {
+                return rule.source_range_start + (value - rule.destination_range_start);
             }
         }
         value
