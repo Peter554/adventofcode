fd3791c296c8e891df4ca2ddea4db573b4748aa6
Peter Byfield <byfield554@gmail.com>
2023-12-05 10:06:06 +0100

    2023 day 5 part 1

---
+++ 2023/rust/aoc/src/day05/mod.rs
@@ -0,0 +1,107 @@
+use anyhow::Result;
+use std::{fs, path::Path};
+
+pub fn part1(input_path: &Path) -> Result<i64> {
+    let input = fs::read_to_string(input_path)?;
+    let input = parse_input(&input);
+    Ok(input
+        .seeds
+        .into_iter()
+        .map(|seed| input.maps.iter().fold(seed, |value, map| map.apply(value)))
+        .min()
+        .unwrap() as i64)
+}
+
+pub fn part2(input_path: &Path) -> Result<i64> {
+    let input = fs::read_to_string(input_path)?;
+    let input = parse_input(&input);
+    let _ = input;
+    Ok(42)
+}
+
+fn parse_input(input: &str) -> Input {
+    let mut it = input.split("\n\n");
+    let seeds = it
+        .next()
+        .unwrap()
+        .strip_prefix("seeds: ")
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let maps = it
+        .map(|s| {
+            let rules = s
+                .lines()
+                .skip(1)
+                .map(|line| {
+                    let line = line
+                        .split_ascii_whitespace()
+                        .map(|s| s.parse().unwrap())
+                        .collect::<Vec<u64>>();
+                    MapRule {
+                        source_range_start: line[1],
+                        destination_range_start: line[0],
+                        range_size: line[2],
+                    }
+                })
+                .collect::<Vec<_>>();
+            Map { rules }
+        })
+        .collect();
+    Input { seeds, maps }
+}
+
+#[derive(Debug)]
+struct Input {
+    seeds: Vec<u64>,
+    maps: Vec<Map>,
+}
+
+#[derive(Debug)]
+struct Map {
+    rules: Vec<MapRule>,
+}
+
+#[derive(Debug)]
+struct MapRule {
+    source_range_start: u64,
+    destination_range_start: u64,
+    range_size: u64,
+}
+
+impl Map {
+    fn apply(&self, value: u64) -> u64 {
+        for rule in self.rules.iter() {
+            if value >= rule.source_range_start && value < rule.source_range_start + rule.range_size
+            {
+                return rule.destination_range_start + (value - rule.source_range_start);
+            }
+        }
+        value
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_part_1() {
+        let input_path = Path::new("./src/day05/sample");
+        assert_eq!(part1(input_path).unwrap(), 35);
+
+        let input_path = Path::new("./src/day05/input");
+        assert_eq!(part1(input_path).unwrap(), 535088217);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input_path = Path::new("./src/day05/sample");
+        assert_eq!(part2(input_path).unwrap(), 42);
+
+        let input_path = Path::new("./src/day05/input");
+        assert_eq!(part2(input_path).unwrap(), 42);
+    }
+}
