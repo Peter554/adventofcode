1e83df76020c7cd03828268b34daa045927698af
Peter Byfield <byfield554@gmail.com>
2023-12-14 23:26:04 +0100

    2023 day 14 part 2

--- 2023/rust/aoc/src/day14/mod.rs
+++ 2023/rust/aoc/src/day14/mod.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use std::{fs, path::Path};
+use std::{collections::HashMap, fs, path::Path};
 
 pub fn part1(input_path: &Path) -> Result<i64> {
     let input = fs::read_to_string(input_path)?;
@@ -10,8 +10,26 @@ pub fn part1(input_path: &Path) -> Result<i64> {
 
 pub fn part2(input_path: &Path) -> Result<i64> {
     let input = fs::read_to_string(input_path)?;
-    let _ = input;
-    Ok(42)
+    let mut map = parse_input(&input);
+
+    let mut hm = HashMap::new();
+    let mut iterations_remaining = 1000000000;
+    while iterations_remaining > 0 {
+        map.tilt_north();
+        map.tilt_west();
+        map.tilt_south();
+        map.tilt_east();
+        iterations_remaining -= 1;
+        if let Some(previous_iterations_remaining) = hm.get(&map) {
+            // Cycle - fast forward!
+            let cycle_length = previous_iterations_remaining - iterations_remaining;
+            iterations_remaining %= cycle_length;
+        } else {
+            hm.insert(map.clone(), iterations_remaining);
+        }
+    }
+
+    Ok(map.load())
 }
 
 fn parse_input(input: &str) -> Map {
@@ -31,13 +49,14 @@ fn parse_input(input: &str) -> Map {
     Map { map }
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 enum Cell {
     Empty,
     RoundRock,
     CubeRock,
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 struct Map {
     map: Vec<Vec<Cell>>,
 }
@@ -57,25 +76,73 @@ impl Map {
     }
 
     fn tilt_north(&mut self) {
-        loop {
-            let mut next_map = self.map.clone();
-            for (row_idx, row) in self.map.iter().enumerate() {
-                if row_idx == self.map.len() - 1 {
-                    break;
-                }
-                for (col_idx, cell) in row.iter().enumerate() {
-                    if matches!(cell, Cell::Empty)
-                        && matches!(next_map[row_idx + 1][col_idx], Cell::RoundRock)
-                    {
-                        next_map[row_idx][col_idx] = Cell::RoundRock;
-                        next_map[row_idx + 1][col_idx] = Cell::Empty;
-                    }
+        for col_idx in 0..self.map[0].len() {
+            let mut drop_idx = 0;
+            for row_idx in 0..self.map.len() {
+                if matches!(self.map[row_idx][col_idx], Cell::RoundRock) {
+                    self.map[row_idx][col_idx] = Cell::Empty;
+                    self.map[drop_idx][col_idx] = Cell::RoundRock;
+                    drop_idx += 1;
+                } else if matches!(self.map[row_idx][col_idx], Cell::CubeRock) {
+                    drop_idx = row_idx + 1;
                 }
             }
-            if next_map == self.map {
-                return;
-            } else {
-                self.map = next_map;
+        }
+    }
+
+    fn tilt_south(&mut self) {
+        for col_idx in 0..self.map[0].len() {
+            let mut drop_idx = self.map.len() - 1;
+            for row_idx in (0..self.map.len()).rev() {
+                if matches!(self.map[row_idx][col_idx], Cell::RoundRock) {
+                    self.map[row_idx][col_idx] = Cell::Empty;
+                    self.map[drop_idx][col_idx] = Cell::RoundRock;
+                    if drop_idx == 0 {
+                        break;
+                    }
+                    drop_idx -= 1;
+                } else if matches!(self.map[row_idx][col_idx], Cell::CubeRock) {
+                    if row_idx == 0 {
+                        break;
+                    }
+                    drop_idx = row_idx - 1;
+                }
+            }
+        }
+    }
+
+    fn tilt_west(&mut self) {
+        for row_idx in 0..self.map.len() {
+            let mut drop_idx = 0;
+            for col_idx in 0..self.map[0].len() {
+                if matches!(self.map[row_idx][col_idx], Cell::RoundRock) {
+                    self.map[row_idx][col_idx] = Cell::Empty;
+                    self.map[row_idx][drop_idx] = Cell::RoundRock;
+                    drop_idx += 1;
+                } else if matches!(self.map[row_idx][col_idx], Cell::CubeRock) {
+                    drop_idx = col_idx + 1;
+                }
+            }
+        }
+    }
+
+    fn tilt_east(&mut self) {
+        for row_idx in 0..self.map.len() {
+            let mut drop_idx = self.map[0].len() - 1;
+            for col_idx in (0..self.map[0].len()).rev() {
+                if matches!(self.map[row_idx][col_idx], Cell::RoundRock) {
+                    self.map[row_idx][col_idx] = Cell::Empty;
+                    self.map[row_idx][drop_idx] = Cell::RoundRock;
+                    if drop_idx == 0 {
+                        break;
+                    }
+                    drop_idx -= 1;
+                } else if matches!(self.map[row_idx][col_idx], Cell::CubeRock) {
+                    if col_idx == 0 {
+                        break;
+                    }
+                    drop_idx = col_idx - 1;
+                }
             }
         }
     }
@@ -98,9 +165,9 @@ mod tests {
     #[test]
     fn test_part2() {
         let input_path = Path::new("./src/day14/sample");
-        assert_eq!(part2(input_path).unwrap(), 42);
+        assert_eq!(part2(input_path).unwrap(), 64);
 
         let input_path = Path::new("./src/day14/input");
-        assert_eq!(part2(input_path).unwrap(), 42);
+        assert_eq!(part2(input_path).unwrap(), 93102);
     }
 }
