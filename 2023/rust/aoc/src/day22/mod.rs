40b5451a2972d95d5cd9ac0d2b557e95e8a5f436
Peter Byfield <byfield554@gmail.com>
2023-12-26 15:58:07 +0100

    2023 day 22 part 2

--- 2023/rust/aoc/src/day22/mod.rs
+++ 2023/rust/aoc/src/day22/mod.rs
@@ -1,13 +1,7 @@
 use anyhow::Result;
 use itertools::Itertools;
 use slotmap::{DefaultKey, SecondaryMap, SlotMap};
-use std::{
-    cmp::Ordering,
-    collections::{HashMap, HashSet},
-    fs,
-    path::Path,
-    usize,
-};
+use std::{cmp::Ordering, collections::HashSet, fs, path::Path, usize};
 
 pub fn part1(input_path: &Path) -> Result<i64> {
     let input = fs::read_to_string(input_path)?;
@@ -15,19 +9,24 @@ pub fn part1(input_path: &Path) -> Result<i64> {
     Ok(tree
         .bricks
         .keys()
-        .filter(|k| can_disintegrate(&tree, k))
+        .filter(|k| can_disintegrate_safely(&tree, k))
         .count() as i64)
 }
 
 pub fn part2(input_path: &Path) -> Result<i64> {
     let input = fs::read_to_string(input_path)?;
-    let _ = input;
-    Ok(42)
+    let tree = build_tree(&input);
+
+    let mut total = 0;
+    for key in tree.bricks.keys() {
+        total += number_of_bricks_falling(&tree, &key);
+    }
+    Ok(total as i64)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Tree {
-    bricks: SlotMap<DefaultKey, ([usize; 2], [usize; 2], usize)>,
+    bricks: SlotMap<DefaultKey, [[usize; 2]; 3]>,
     supports: SecondaryMap<DefaultKey, HashSet<DefaultKey>>,
     supported_by: SecondaryMap<DefaultKey, HashSet<DefaultKey>>,
 }
@@ -59,27 +58,29 @@ fn build_tree(input: &str) -> Tree {
     let mut supports = SecondaryMap::new();
     let mut supported_by = SecondaryMap::new();
 
-    let mut heights: HashMap<DefaultKey, usize> = HashMap::new();
     while let Some(brick_to_drop) = bricks_to_drop.pop() {
-        let brick = (
+        let brick = [
             brick_to_drop[0],
             brick_to_drop[1],
-            brick_to_drop[2][1] - brick_to_drop[2][0] + 1,
-        );
+            [0, 0], // Correct this later.
+        ];
         let brick_key = bricks.insert(brick);
 
         let mut supported_by_keys = vec![];
-        let mut supported_at_height = 0usize;
-        for (other_brick_key, other_brick_height) in heights.iter() {
-            let other_brick = bricks.get(*other_brick_key).unwrap();
-            if overlaps(brick.0, other_brick.0) && overlaps(brick.1, other_brick.1) {
+        let mut supported_at_height = 0;
+        for (other_brick_key, other_brick) in bricks.iter() {
+            if other_brick_key == brick_key {
+                continue;
+            }
+            let other_brick_height = other_brick[2][1];
+            if overlaps(brick[0], other_brick[0]) && overlaps(brick[1], other_brick[1]) {
                 match other_brick_height.cmp(&supported_at_height) {
                     Ordering::Greater => {
-                        supported_by_keys = vec![*other_brick_key];
-                        supported_at_height = *other_brick_height;
+                        supported_by_keys = vec![other_brick_key];
+                        supported_at_height = other_brick_height;
                     }
                     Ordering::Equal => {
-                        supported_by_keys.push(*other_brick_key);
+                        supported_by_keys.push(other_brick_key);
                     }
                     Ordering::Less => continue,
                 }
@@ -97,7 +98,11 @@ fn build_tree(input: &str) -> Tree {
             brick_key,
             supported_by_keys.into_iter().collect::<HashSet<_>>(),
         );
-        heights.insert(brick_key, supported_at_height + brick.2);
+        let brick = bricks.get_mut(brick_key).unwrap();
+        brick[2] = [
+            supported_at_height + 1,
+            supported_at_height + 1 + brick_to_drop[2][1] - brick_to_drop[2][0],
+        ];
     }
 
     Tree {
@@ -111,11 +116,37 @@ fn overlaps(a: [usize; 2], b: [usize; 2]) -> bool {
     a[0] <= b[1] && b[0] <= a[1]
 }
 
-fn can_disintegrate(tree: &Tree, brick_key: &DefaultKey) -> bool {
-    match &tree.supports.get(*brick_key) {
+fn can_disintegrate_safely(tree: &Tree, brick_key: &DefaultKey) -> bool {
+    match tree.supports.get(*brick_key) {
         Some(supported) => supported.iter().all(|k| tree.supported_by[*k].len() > 1),
         None => true,
     }
+}
+
+fn number_of_bricks_falling(tree: &Tree, brick_to_disintegrate: &DefaultKey) -> usize {
+    let mut removed_keys = HashSet::new();
+    removed_keys.insert(*brick_to_disintegrate);
+    for higher_brick_key in tree
+        .bricks
+        .iter()
+        .filter(|(_, b)| b[2][0] > tree.bricks[*brick_to_disintegrate][2][1])
+        // Topological order.
+        .sorted_by_key(|(_, b)| b[2][0])
+        .map(|(k, _)| k)
+    {
+        if tree
+            .supported_by
+            .get(higher_brick_key)
+            .unwrap()
+            .difference(&removed_keys)
+            .count()
+            == 0
+        {
+            // Brick is no longer supported.
+            removed_keys.insert(higher_brick_key);
+        }
+    }
+    removed_keys.len() - 1
 }
 
 #[cfg(test)]
@@ -135,9 +166,9 @@ mod tests {
     #[test]
     fn test_part2() {
         let input_path = Path::new("./src/day22/sample");
-        assert_eq!(part2(input_path).unwrap(), 42);
+        assert_eq!(part2(input_path).unwrap(), 7);
 
         let input_path = Path::new("./src/day22/input");
-        assert_eq!(part2(input_path).unwrap(), 42);
+        assert_eq!(part2(input_path).unwrap(), 74594);
     }
 }
